d227057e55bcd0f638fb635e606a44985c7ad440
Saumya Saxena <saumyasaxena54@gmail.com>
2025-01-26 18:32:13 +0530

    Initial commit: React Native project setup

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import CircleScreen from '../screens/CircleScreen';
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Circle" component={CircleScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
